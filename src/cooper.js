b2a7a1e7243db097b47185518d3655f1812bbf33
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-25 14:15:13 +0200

    added cooper spec file

--- src/cooper.js
+++
@@ -1,3 +0,0 @@
-function CooperTest (distance) {
-
-}
--- src/person.js
+++
@@ -1,8 +0,0 @@
-function Person(attr) {
-    this.gender = attr.gender;
-    this.age = attr.age;
-};
-
-Person.prototype.cooperAssesment = function (distance) {
-    this.cooperMessage = assmentCooper(this, distance);
-}
